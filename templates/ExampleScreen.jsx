7e26bc88fd5208bf7e1cb3dbab01e92b662ed742
Renato Steinberg <7071496+renatost@users.noreply.github.com>
2021-04-14 14:07:13 -0700

    Changes to example miniapp

--- templates/ExampleScreen.jsx
+++ templates/ExampleScreen.jsx
@@ -2,18 +2,8 @@ exports.Template = (data, context) => {
 
     return (
         <Klutch.KScreen>
-            <Klutch.KHeader showBackArrow>Subscription</Klutch.KHeader>
-            <Klutch.KText style={{ textAlign: "center" }}>Add subscriptions to track by going to the transaction detail page and pressing the MiniApp panel. Example below</Klutch.KText>
-            
-            <Native.View style={{ flex: 1, justifyContent: "center" }} >
-                <Native.Image source={{ uri: "https://klutch-test-public.s3.amazonaws.com/phone.png" }} style={{ height: 400 }} resizeMode="contain" />
-            </Native.View>
-            
-            <Klutch.KText style={{ marginVertical: 30, textAlign: "center" }}>Our AI will also look for subscriptions as you spend and will start adding them here.</Klutch.KText>
-            
-            <Klutch.KButtonBar >
-                <Klutch.KButton type="primary" label="GO TO TRANSACTIONS" onPress={() => null} />
-            </Klutch.KButtonBar>
+            <Klutch.KHeader showBackArrow>My MiniApp</Klutch.KHeader>
+            <Klutch.KText style={{ textAlign: "center" }}>This is a miniapp template!</Klutch.KText>            
         </Klutch.KScreen>
     )
 }
