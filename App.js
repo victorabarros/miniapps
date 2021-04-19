8f6282f4ad4acd19724e33e8b4dbe80655a336d3
Renato Steinberg <renato@alloycard.com>
2021-04-19 16:34:41 -0700

    MiniApp improvements

--- App.js
+++ App.js
@@ -5,12 +5,40 @@ import AppLoading from 'expo-app-loading';
 import SimulationComponent from './SimulationComponent';
 import {KText, KButton, KScreen} from "@alloycard/klutch-components"
 
+
+
+
+/* MINIAPP CONFIGURATION */
+
+
+const APPNAME = "Demo MiniApp"
+
+const Templates = {
+  Main: {type: "fullscreen", template: require(`./dist/templates/Main.template`)},
+  Transaction: {type: "transaction", template: require(`./dist/templates/Transaction.template`)},
+  Home: {type: "home", template: require(`./dist/templates/Home.template`)},
+}
+
+const initialData = {
+  transaction: {
+      id: "0d2b05d9-68fd-4dd3-a354-25199bd55d48",
+      merchantName: "Sport's Academy",
+      cardName: "Shopping Card",
+      amount: 453.20,
+      category: "SHOPPING",
+      date: new Date("2021-03-25T11:43:23Z")                
+  }    
+}
+
+/* END MINIAPP CONFIGURATION */
+
+
 export default function App() {
 
   let [fontsLoaded] = useFonts({Inter_400Regular, Inter_600SemiBold, Inter_700Bold});
   const [templateToLoad, setTemplateToLoad]  = useState()
 
-  const APPNAME = "Demo MiniApp"
+
 
   if (!fontsLoaded) {
     return <AppLoading />;
@@ -21,18 +49,28 @@ export default function App() {
   
 
     return (
-      <KScreen style={{flex: 1, marginVertical: 30, justifyContent: 'space-evenly'}}>
+      <KScreen style={{flex: 1, marginVertical: 30, justifyContent: 'space-evenly'}}>        
         <KText>What template do you want to open?</KText>
-        <KButton type="primary" style={buttonStyle} label="Main" onPress={() => setTemplateToLoad({type: "fullscreen", template: require(`./dist/templates/Main.template`)})} />
-        <KButton type="primary" style={buttonStyle} label="Transaction Panel" onPress={() => setTemplateToLoad({type: "transaction", template: require(`./dist/templates/Transaction.template`)})} />
-        <KButton type="primary" style={buttonStyle} label="Home Panel" onPress={() => setTemplateToLoad({type: "home", template: require(`./dist/templates/Home.template`)})} />      
+        <KButton type="primary" style={buttonStyle} label="Main" onPress={() => setTemplateToLoad(Templates.Main)} />
+        <KButton type="primary" style={buttonStyle} label="Transaction Panel" onPress={() => setTemplateToLoad(Templates.Transaction)} />
+        <KButton type="primary" style={buttonStyle} label="Home Panel" onPress={() => setTemplateToLoad(Templates.Home)} />      
       </KScreen>
     )  
   }
   
+  const onLoadTemplate = (templateName, templateData) => {
+    setTemplateData(templateData)  
+    setTemplateToLoad(templateName)    
+  }
+  
   return (
     <View>
-      <SimulationComponent name={APPNAME} type={templateToLoad.type} template={templateToLoad.template} />
+      <SimulationComponent 
+        name={APPNAME} 
+        type={templateToLoad.type} 
+        template={templateToLoad.template} 
+        data={templateData}
+        onLoadTemplate={onLoadTemplate} />
     </View>
   );
 }
--- SimulationComponent.jsx
+++ SimulationComponent.jsx
@@ -1,24 +1,23 @@
-import React, { useEffect, useState } from "react"
-import * as Native from "react-native"
-import { useAssets, Asset } from 'expo-asset';
-import {Text} from "react-native";
-import * as FileSystem from 'expo-file-system';
-import * as Klutch from '@alloycard/klutch-components'
+import * as Klutch from '@alloycard/klutch-components';
+import { Asset } from 'expo-asset';
+import React, { useEffect, useState } from "react";
+import * as Native from "react-native";
 
-const SimulationComponent =  ({template, type, name}) => {
+const SimulationComponent =  ({template, type, name, data, onLoadTemplate}) => {
     
-    const [templateAsset] = useAssets([template])    
     const [content, setContent] = useState(null)
-    
-    useEffect(() => {
+            
+    useEffect(() => {        
         const run = async () => {
-            if(templateAsset) {
-                const doc = await FileSystem.readAsStringAsync(templateAsset[0].localUri)               
+            if(template) {                
+                const mod = await Asset.loadAsync(template)
+                const resp = await fetch(mod[0].uri);                            
+                const doc = await resp.text()               
                 setContent(doc)  
             }
         }
         run()
-    }, [templateAsset])
+    })
 
     const react = React
     const native = Native
@@ -27,11 +26,17 @@ const SimulationComponent =  ({template, type, name}) => {
         return <Klutch.KText>Loading...</Klutch.KText>
     }
     
+    const simulationContext = {
+        loadTemplate(templateName, templateData) {
+            onLoadTemplate &&  onLoadTemplate(templateName, templateData)
+        }
+    }
+
     const drawTemplate = function() {
         const r = eval(content)
         const React = react
         const Native = native
-        return r()
+        return r(data, simulationContext)
     }
 
     if (type === "fullscreen") {
@@ -55,3 +60,6 @@ const SimulationComponent =  ({template, type, name}) => {
 }
 
 export default SimulationComponent
+
+
+
