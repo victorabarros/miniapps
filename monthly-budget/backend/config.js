cefb72cfa9b9650c3440f890ba2162181a1304df
VictorBarros <victorbarros1130@gmail.com>
2021-10-12 14:36:39 -0300

    health check endpoint

--- monthly-budget/backend/config.js
+++ monthly-budget/backend/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? ".env.test" : ".env" })
+const throwMissingVariable = (name) => { throw new Error(`missing required env variable "${name}"`) }
 
 const config = {
+  klutchServerUrl: process.env.KLUTCH_SERVER_URL || throwMissingVariable('KLUTCH_SERVER_URL'),
   port: process.env.PORT || 3003,
 }
 
--- monthly-budget/backend/src/routes.js
+++ monthly-budget/backend/src/routes.js
@@ -1,10 +1,31 @@
+const axios = require('axios')
 const { Router } = require("express")
 const httpStatus = require('http-status')
+const { klutchServerUrl } = require("../config")
 
 const router = Router()
 
-router.get("/", async (req, resp) => {
-  return resp.status(httpStatus.OK).json({ foo: 'bar' })
+router.get("/health", async (req, resp) => {
+  let responseStatus = httpStatus.OK
+  let services = {
+    klutchServer: {
+      success: true,
+      errorMessage: null,
+    },
+  }
+
+  await axios({ method: 'get', url: `${klutchServerUrl}/healthcheck`, })
+    .catch(function (error) {
+      services.klutchServer.success = false
+      services.klutchServer.errorMessage = "klutch server comunication fail"
+      responseStatus = httpStatus.SERVICE_UNAVAILABLE
+      console.log(services.klutchServer.errorMessage, error)
+    })
+
+  // TODO
+  // check database
+
+  return resp.status(responseStatus).json({ services })
 })
 
 module.exports = { router }
