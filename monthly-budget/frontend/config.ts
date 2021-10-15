32b27b65d986978b499c17d3f372807e0822da10
VictorBarros <victorbarros1130@gmail.com>
2021-10-15 14:18:25 -0300

    fix server port

--- monthly-budget/frontend/config.ts
+++ monthly-budget/frontend/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   recipe: {
-    serverUrl: process.env.REACT_APP_SERVER_URL || "http://192.168.15.6:3002",
+    serverUrl: process.env.REACT_APP_SERVER_URL || "http://192.168.15.6:3003",
   }
 }
