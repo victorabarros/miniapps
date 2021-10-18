871582ff3a84394edd68729e5a0c349cc06432c8
VictorBarros <victorbarros1130@gmail.com>
2021-10-18 18:39:26 -0300

    refact styles

--- monthly-budget/frontend/templates/Main.jsx
+++ monthly-budget/frontend/templates/Main.jsx
@@ -1,7 +1,10 @@
 const styles = {
   loading: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "center",
+  },
+  textHeader: {
+    fontSize: 20,
   },
   editButtonContainer: {
     position: 'absolute',
@@ -16,55 +19,78 @@ const styles = {
     alignSelf: 'flex-end',
   },
   summaryContainer: {
-    marginVertical: 5
+    marginVertical: 5,
   },
   summaryAmount: {
     fontSize: 45,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   summarySubtitle: {
     fontWeight: 'bold',
-    marginVertical: 10
+    marginVertical: 10,
   },
 }
 
-const budgetContainer = ({ id, category, amount: budget, spent }) => {
-  return (
-    <Klutch.KView style={{ marginVertical: 10 }} key={`budget-${id}`}>
-      <Klutch.KView style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-
-        <Klutch.KView>
-          <Klutch.KText style={{ fontSize: 20, fontWeight: 'bold', }}>{category.toUpperCase()}</Klutch.KText>
-          <Klutch.KText style={{ fontSize: 15 }}>
-            {Math.max((budget - spent), 0).toFixed(2)}
-            <Klutch.KText style={{ fontSize: 10 }}> LEFT</Klutch.KText>
-          </Klutch.KText>
-        </Klutch.KView>
-
-        <Klutch.KView style={{ alignSelf: 'flex-end' }}>
-          <Klutch.KText style={{ fontSize: 15 }}>{spent.toFixed(2)}
-            <Klutch.KText style={{ fontSize: 10 }}> SPENT</Klutch.KText>
-          </Klutch.KText>
-          <Klutch.KText style={{ fontSize: 10, alignSelf: 'flex-end', }}>{`OF ${budget}`}</Klutch.KText>
-        </Klutch.KView>
-      </Klutch.KView>
-
-      <Klutch.KView style={{
-        height: 6,
-        width: '100%',
-        borderWidth: .5,
-        marginVertical: 5
-      }}>
-        <Klutch.KView style={{
-          height: '100%',
-          backgroundColor: '#44CCFF',
-          width: `${100 * spent / budget}%`,
-        }} />
-      </Klutch.KView>
-
-    </Klutch.KView>
-  )
+const budgetContainerStyles = {
+  container: {
+    marginVertical: 10,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  category: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  currency: {
+    fontSize: 15,
+  },
+  text: {
+    fontSize: 10,
+  },
+  budget: {
+    fontSize: 10,
+    alignSelf: 'flex-end',
+  },
+  progressBar: {
+    height: 6,
+    width: '100%',
+    borderWidth: .5,
+    marginVertical: 5,
+  },
+  progress: {
+    height: '100%',
+    backgroundColor: '#44CCFF',
+  },
 }
+
+const budgetContainer = ({ id, category, amount: budget, spent }) => (
+  <Klutch.KView style={budgetContainerStyles.container} key={`budget-${id}`}>
+    <Klutch.KView style={budgetContainerStyles.headerContainer}>
+
+      <Klutch.KView>
+        <Klutch.KText style={budgetContainerStyles.category}>{category.toUpperCase()}</Klutch.KText>
+        <Klutch.KText style={budgetContainerStyles.currency}>
+          {Math.max((budget - spent), 0).toFixed(2)}
+          <Klutch.KText style={budgetContainerStyles.text}> LEFT</Klutch.KText>
+        </Klutch.KText>
+      </Klutch.KView>
+
+      <Klutch.KView style={{ alignSelf: 'flex-end' }}>
+        <Klutch.KText style={budgetContainerStyles.currency}>{spent.toFixed(2)}
+          <Klutch.KText style={budgetContainerStyles.text}> SPENT</Klutch.KText>
+        </Klutch.KText>
+        <Klutch.KText style={budgetContainerStyles.budget}>{`OF ${budget}`}</Klutch.KText>
+      </Klutch.KView>
+    </Klutch.KView>
+
+    <Klutch.KView style={budgetContainerStyles.progressBar}>
+      <Klutch.KView style={[budgetContainerStyles.progress, { width: `${100 * spent / budget}%` }]} />
+    </Klutch.KView>
+
+  </Klutch.KView>
+)
 
 Template = (data, context) => {
   let { budgets, loading, totalBudget } = context.state || { budgets: [], loading: true }
@@ -88,11 +114,7 @@ Template = (data, context) => {
     <Klutch.KView key='container'>
 
       <Klutch.KView key='header'>
-        <Klutch.KHeader
-          showBackArrow
-          onBackArrowPressed={context.closeMiniApp}
-          textStyle={{ fontSize: 20 }}
-        >
+        <Klutch.KHeader showBackArrow onBackArrowPressed={context.closeMiniApp} textStyle={styles.textHeader} >
           MONTHLY BUDGET
         </Klutch.KHeader>
 
@@ -102,9 +124,7 @@ Template = (data, context) => {
             console.log("TODO move to edit page")
           }}
         >
-          <Klutch.KText style={styles.editButtonText}>
-            EDIT
-          </Klutch.KText>
+          <Klutch.KText style={styles.editButtonText}>EDIT</Klutch.KText>
         </Klutch.KPressable>
       </Klutch.KView>
 
@@ -114,12 +134,9 @@ Template = (data, context) => {
       </Klutch.KView >
 
       <Klutch.KScrollView key='body'>
-
-        <Klutch.KView>
-          {budgets.map(budgetContainer)}
-        </Klutch.KView>
-
+        {budgets.map(budgetContainer)}
       </Klutch.KScrollView>
+
     </Klutch.KView >
   )
 }
