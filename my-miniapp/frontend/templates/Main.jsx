fb93f5b60625d45ffb2cb5dbda78ba5a364bda5e
VictorBarros <victorbarros1130@gmail.com>
2021-11-15 12:41:44 -0300

    lint

--- my-miniapp/frontend/templates/Main.jsx
+++ my-miniapp/frontend/templates/Main.jsx
@@ -1,10 +1,9 @@
 Template = (data, context) => {
 
-    return (
-        <>
-            <Klutch.KHeader showBackArrow>My MiniApp</Klutch.KHeader>
-            <Klutch.KText style={{ textAlign: "center" }}>This is a miniapp template!</Klutch.KText>            
-        </>
-    )
+  return (
+    <Klutch.KView style={{ flex: 1 }}>
+      <Klutch.KHeader showBackArrow>My MiniApp</Klutch.KHeader>
+      <Klutch.KText style={{ textAlign: "center" }}>This is a miniapp template!</Klutch.KText>
+    </Klutch.KView>
+  )
 }
-
