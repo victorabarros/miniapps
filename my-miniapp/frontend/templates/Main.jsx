12c668bb59c5e8e8c03766b6d6f266d66f75ca2e
VictorBarros <victorbarros1130@gmail.com>
2021-11-15 16:27:19 -0300

    main page template

--- my-miniapp/frontend/templates/Main.jsx
+++ my-miniapp/frontend/templates/Main.jsx
@@ -10,7 +10,24 @@ Template = (data, context) => {
   return (
     <Klutch.KView style={{ flex: 1 }}>
       <Klutch.KHeader showBackArrow>My MiniApp</Klutch.KHeader>
-      <Klutch.KText style={{ textAlign: "center" }}>This is a miniapp template!</Klutch.KText>
-    </Klutch.KView>
+
+      {(resources || []).map(({ id, name, value }) => (
+        <Klutch.KView key={`resource-${id}`} style={{ height: 50, borderWidth: .8, marginVertical: 5 }}>
+          <Klutch.KView style={{ backgroundColor: 'lightblue', height: "100%", position: "absolute", width: `${100 * value}%` }} />
+          <Klutch.KView
+            style={{ flex: 1, padding: 5, flexDirection: 'row', alignItems: "center", justifyContent: "space-between" }}>
+            <Klutch.KText>{name}</Klutch.KText>
+            <Klutch.KText>{`${value * 100}%`}</Klutch.KText>
+          </Klutch.KView>
+        </Klutch.KView>
+      ))}
+
+      <Klutch.KPressable
+        style={{ height: 50, borderWidth: .5, marginVertical: 5, padding: 5, flexDirection: 'row', alignItems: "center", justifyContent: "center" }}
+        onPress={() => console.log("Move to add resource page")}
+      >
+        <Klutch.PlusSign color={Klutch.KlutchTheme.colors.secondary} />
+      </Klutch.KPressable>
+    </Klutch.KView >
   )
 }
